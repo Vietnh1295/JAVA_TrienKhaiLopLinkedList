2da8f636c7acdf6b2cd07626ae921929da64666d
Your Name <you@example.com>
2019-07-05 09:34:42 +0700

    add

---
+++ src/MyLinkedList.java
@@ -0,0 +1,40 @@
+public class MyLinkedList {
+    private Node head;
+    private int numNodes;
+
+    public MyLinkedList(Object data) {
+        head = new Node(data);
+    }
+
+    public void add(int index, Object data) {
+        Node temp = head;
+        Node holder;
+        for (int i = 0; i < index - 1 && temp.getNext() != null; i++) {
+                temp=temp.getNext();
+        }
+        holder=temp.getNext();
+        temp.setNext(new Node(data));
+        temp.getNext().setNext(holder);
+        numNodes++;
+    }
+    public void addFirst(Object data) {
+        Node temp = head;
+        head = new Node(data);
+        head.setNext(temp);
+        numNodes++;
+    }
+    public Node get(int index){
+        Node temp=head;
+        for(int i=0; i<index; i++) {
+            temp = temp.getNext();
+        }
+        return temp;
+    }
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.getData());
+            temp = temp.getNext();
+        }
+    }
+}
---
+++ src/MyLinkedListTest.java
@@ -0,0 +1,10 @@
+public class MyLinkedListTest {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.add(4, 9);
+        ll.printList();
+    }
+}
+
